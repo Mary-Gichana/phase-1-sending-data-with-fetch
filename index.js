fd0f08db8f9358c56d291fe49cbacaaf25527052
Mary Monchari Gichana <marygichana95@gmail.com>
2024-10-09 14:34:47 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+function submitData(username, useremail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: username,
+      email: useremail,
+    }),
+  })
+    .then((response) => response.json())
+    .then((Object) => {
+      const p = document.createElement("p");
+      p.textContent = `User ID: ${Object.id}`;
+      document.body.appendChild(p);
+    })
+    .catch((error) => {
+      const p = document.createElement("p");
+      p.textContent = `Error: ${error.message}`;
+      document.body.appendChild(p);
+    });
+}
